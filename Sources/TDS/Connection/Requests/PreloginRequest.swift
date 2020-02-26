24f57d74393929e64af0ee2b7c54948a6ee8f68b
Aaron Edwards <aaron.j.edwards@me.com>
2020-02-26 13:30:21 -0700

    Added new `TDSMessage` initialized to create message from TDSPacketType

--- Sources/TDS/Connection/Requests/PreloginRequest.swift
+++ Sources/TDS/Connection/Requests/PreloginRequest.swift
@@ -34,9 +34,8 @@ private final class PreloginRequest: TDSRequest {
             if let enc = message.body.encryption {
                 switch enc {
                 case .encryptOn, .encryptReq, .encryptClientCertOn, .encryptClientCertReq:
-                    let outbound = try TDSPacket(message: TDSMessages.SSLKickoff(), isLastPacket: true, allocator: allocator)
-                    
-                    return TDSMessage(packets: [outbound])
+                    let message = try TDSMessage.init(packetType: TDSMessages.SSLKickoff(), allocator: allocator)
+                    return message
                 default:
                     throw TDSError.protocol("PRELOGIN Error: Server does not supprt encryption.")
                 }
@@ -49,9 +48,8 @@ private final class PreloginRequest: TDSRequest {
     }
     
     func start(allocator: ByteBufferAllocator) throws -> TDSMessage {
-        let message = TDSMessages.PreloginPacket(version: "9.0.0", encryption: .encryptOn)
-        let packet = try TDSPacket(message: message, isLastPacket: true, allocator: allocator)
-        
-        return TDSMessage(packets: [packet])
+        let prelogin = TDSMessages.PreloginPacket(version: "9.0.0", encryption: .encryptOn)
+        let message = try TDSMessage.init(packetType: prelogin, allocator: allocator)
+        return message
     }
 }
