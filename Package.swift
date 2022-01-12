fd22c81f798e7b125d56e753d3168a769ac290b3
copterdoctor <Jordananders@me.com>
2022-01-12 10:26:21 +1000

    Updated package.swift to include iOS, Updated package dependencies to latest Minor, Modified Login7Message to support iOS hostname

--- Package.swift
+++ Package.swift
@@ -1,19 +1,20 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "swift-tds",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+       .iOS("13.0")
     ],
     products: [
         .library(name: "TDS", targets: ["TDS"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.0.0"),
-        .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0" ..< "3.0.0"),
-        .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/apple/swift-metrics.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         .target(name: "TDS", dependencies: [
